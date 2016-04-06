8ea1c2cb69ab7b39318cbcab36939348a6c8b3fd
ping2010get@gmail.com <Roking>
2016-04-06 22:16:42 +0800

    first commit

---
+++ idealizedPing/src/main/java/org/idealized/App.java
@@ -0,0 +1,13 @@
+package org.idealized;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
