a32779be4e1640c6b83f732a8c2f74c724cc39df
ping2010get@gmail.com <RoKing@RoKing-PC>
2016-04-06 22:39:35 +0800

    test

--- idealizedPing/src/main/java/org/rk/App.java
+++ idealizedPing/src/main/java/org/rk/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello idealizedPing!" );
+        System.out.println( "Hello luoping!" );
     }
 }
